2731d929b8cf23f61c8c6dba14232d096f448259
MKoponirov <54912625+koponirov@users.noreply.github.com>
2024-02-22 17:06:48 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+// service-worker.js
+
+self.addEventListener('install', function(event) {
+  console.log('Service worker installed');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service worker activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch intercepted:', event.request.url);
+});
